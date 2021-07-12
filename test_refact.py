92fb1cc4a3bdbf5ada086b14fc05fe1b560d1217
eeplearng <eeplearning@unist.ac.kr>
2021-07-12 23:18:32 +0900

    (Refact) just take out functions form the main function

--- test_refact.py
+++ test_refact.py
@@ -14,6 +14,38 @@ import os
 #####args
 from collections import defaultdict
 
+
+def accuracy(predictions, targets):
+   predictions = predictions.argmax(dim=1).view(targets.shape)
+   return (predictions == targets).sum().float() / targets.size(0)
+
+
+def fast_adapt(batch, learner, loss, adaptation_steps, shots, ways, device):
+   data, labels = batch
+   data, labels = data.to(device), labels.to(device)
+   
+   # Separate data into adaptation/evalutation sets
+   adaptation_indices = np.zeros(data.size(0), dtype=bool)
+   adaptation_indices[np.arange(shots * ways) * 2] = True
+   evaluation_indices = torch.from_numpy(~adaptation_indices)
+   adaptation_indices = torch.from_numpy(adaptation_indices)
+   # print("evaluation_indices",evaluation_indices)
+   # print("adaptation_indices", adaptation_indices)
+   
+   adaptation_data, adaptation_labels = data[adaptation_indices], labels[adaptation_indices]
+   evaluation_data, evaluation_labels = data[evaluation_indices], labels[evaluation_indices]
+   
+   # Adapt the model
+   for step in range(adaptation_steps):
+      train_error = loss(learner(adaptation_data), adaptation_labels)
+      learner.adapt(train_error)
+   
+   # Evaluate the adapted model
+   predictions = learner(evaluation_data)
+   valid_error = loss(predictions, evaluation_labels)
+   valid_accuracy = accuracy(predictions, evaluation_labels)
+   return valid_error, valid_accuracy
+
 #print("local 수정 이제 자동으로 안되나?")
 
 def maml_exp(ways=5,
@@ -62,39 +94,6 @@ def maml_exp(ways=5,
    ####csv
 
    result = defaultdict(list)
-   
-
-   def accuracy(predictions, targets):
-       predictions = predictions.argmax(dim=1).view(targets.shape)
-       return (predictions == targets).sum().float() / targets.size(0)
-   
-   
-   def fast_adapt(batch, learner, loss, adaptation_steps, shots, ways, device):
-       data, labels = batch
-       data, labels = data.to(device), labels.to(device)
-   
-       # Separate data into adaptation/evalutation sets
-       adaptation_indices = np.zeros(data.size(0), dtype=bool)
-       adaptation_indices[np.arange(shots*ways) * 2] = True
-       evaluation_indices = torch.from_numpy(~adaptation_indices)
-       adaptation_indices = torch.from_numpy(adaptation_indices)
-       #print("evaluation_indices",evaluation_indices)
-       #print("adaptation_indices", adaptation_indices)
-   
-       adaptation_data, adaptation_labels = data[adaptation_indices], labels[adaptation_indices]
-       evaluation_data, evaluation_labels = data[evaluation_indices], labels[evaluation_indices]
-   
-       # Adapt the model
-       for step in range(adaptation_steps):
-           train_error = loss(learner(adaptation_data), adaptation_labels)
-           learner.adapt(train_error)
-   
-       # Evaluate the adapted model
-       predictions = learner(evaluation_data)
-       valid_error = loss(predictions, evaluation_labels)
-       valid_accuracy = accuracy(predictions, evaluation_labels)
-       return valid_error, valid_accuracy
-   
    
    import timeit
    
