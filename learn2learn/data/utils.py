3bd2aeaf044bc48d8f08d61613035db4605ab54c
eeplearng <eeplearning@unist.ac.kr>
2021-07-31 22:32:34 +0900

    (New) add Fake ePisode : --fp

--- learn2learn/data/utils.py
+++ learn2learn/data/utils.py
@@ -2,6 +2,7 @@
 
 import requests
 import tqdm
+import time
 
 CHUNK_SIZE = 1 * 1024 * 1024
 
@@ -23,11 +24,18 @@ def download_file(source, destination, size=None):
 def download_file_from_google_drive(id, destination):
     URL = "https://docs.google.com/uc?export=download"
     session = requests.Session()
-    response = session.get(URL, params={'id': id}, stream=True)
+    try:
+        response = session.get(URL, params={'id': id}, stream=True)
+    except:
+        time.sleep(1)
+        response = session.get(URL, params={'id': id}, stream=True)
+       
+       
+        
     token = get_confirm_token(response)
     if token:
         params = {'id': id, 'confirm': token}
-        response = session.get(URL, params=params, stream=True)
+        response = session.get(URL, params=params, stream=True )
     save_response_content(response, destination)
 
 
